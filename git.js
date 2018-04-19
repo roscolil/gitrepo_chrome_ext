5607047fb99c8b29350c305943a69625a882ca77
roscolil <ross.lillis@gmail.com>
2018-04-19 15:29:37 +1000

    added html

--- git.js
+++ git.js
@@ -1,13 +1,18 @@
-function outputPageContent() {
-  if (repositories.length == 0) {
-    outhtml = outhtml + '<p>No repos!</p></div>';
-  }
-  else {
-    outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
-    $.each(repositories, function(index) {
-      outhtml = outhtml + '<li><a href="' + repositories[index].html_url + '" target="_blank">' + repositories[index].name + '</a></li>';
-    });
-    outhtml = outhtml + '</ul></div>';
-  }
-  $('#ghapidata').html(outhtml);
-} 
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.create({url: 'index.html'});
+  });
+
+
+// function outputPageContent() {
+//   if (repositories.length == 0) {
+//     outhtml = outhtml + '<p>No repos!</p></div>';
+//   }
+//   else {
+//     outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
+//     $.each(repositories, function(index) {
+//       outhtml = outhtml + '<li><a href="' + repositories[index].html_url + '" target="_blank">' + repositories[index].name + '</a></li>';
+//     });
+//     outhtml = outhtml + '</ul></div>';
+//   }
+//   $('#ghapidata').html(outhtml);
+// }
