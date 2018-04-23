c5511f06dedec9642fe37bc9614c261159ead79d
roscolil <ross.lillis@gmail.com>
2018-04-23 15:29:58 +1000

    added css file

--- git.js
+++ git.js
@@ -2,24 +2,16 @@
 //     var newUrl = "http://github.com/";
 //     chrome.tabs.create({url: newUrl});
 //   });
-var btn = document.querySelector('button');
 
-btn.addEventListener('click', function () {
-  var newUrl = "http://github.com/";
-  chrome.tabs.create({url: newUrl});
+// var btn = document.querySelector('button');
+//
+// btn.addEventListener('click', function () {
+//   var newUrl = "http://github.com/";
+//   chrome.tabs.create({url: newUrl});
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', function() {
+    chrome.tabs.create({url: 'http://github.com/'});
+  });
 });
-
-
-// function outputPageContent() {
-//   if (repositories.length == 0) {
-//     outhtml = outhtml + '<p>No repos!</p></div>';
-//   }
-//   else {
-//     outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
-//     $.each(repositories, function(index) {
-//       outhtml = outhtml + '<li><a href="' + repositories[index].html_url + '" target="_blank">' + repositories[index].name + '</a></li>';
-//     });
-//     outhtml = outhtml + '</ul></div>';
-//   }
-//   $('#ghapidata').html(outhtml);
-// }
